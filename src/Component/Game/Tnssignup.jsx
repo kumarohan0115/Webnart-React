0b9a171e2e925be6798f23e7a752cfdb4d133030
kumarohan0115 <rohankr350@gmail.com>
2022-10-02 20:37:31 +0530

    form done

--- src/Component/Game/Tnssignup.jsx
+++ src/Component/Game/Tnssignup.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
+import './react-form.css'
 import { useState } from 'react'
 import axios from '../../axios'
+import Navbar from '../../Component/Navbar/Navbar'
 
 const Tnssignup = () => {
 
   const [user, setUser]= useState({
-    username:"",
-    password:"",
-    role:""
+    fullname:"",
+    branch:"",
+    year:"",
+    email:"",
+    phone:"",
+    preknowledge:"",
+    select:""
   })
 
   const register=()=>{
     console.log(user);
     axios.post('/register', {
-      username:user.username,
-    password:user.password,
-    role:user.role
+      fullname:user.fullname,
+      branch:user.branch,
+      year:user.year,
+      email:user.email,
+      phone:user.phone,
+      preknowledge:user.preknowledge,
+      select:user.Select
   })
   .then(function (response) {
     if(response==="Successfull!"){
@@ -38,42 +48,65 @@ const Tnssignup = () => {
     setUser({...user,[name]:value});
   }
 
-  const postdata=async(e)=>{
-    e.preventDefault()
-    const {username, password, role} = user;
-    const res = await fetch("/register",{
-      headers:{
-        "content-type":"application/json"
-      },
-      body:JSON.stringify({
-        username, password, role
-      })
-    })
-    const data = await res.json();
-    if(data.status ===422 || !data){
-      window.alert("Invalid Registration")
-      console.log("Invalid Registration")
-    }
-    else{
-      window.alert(" Registration Sucessfull!")
-      console.log("Registration Sucessfull")
-    }
-  }
-
-
+  // const postdata=async(e)=>{
+  //   e.preventDefault()
+  //   const {username, password, role} = user;
+  //   const res = await fetch("/register",{
+  //     headers:{
+  //       "content-type":"application/json"
+  //     },
+  //     body:JSON.stringify({
+  //       username, password, role
+  //     })
+  //   })
+  //   const data = await res.json();
+  //   if(data.status ===422 || !data){
+  //     window.alert("Invalid Registration")
+  //     console.log("Invalid Registration")
+  //   }
+  //   else{
+  //     window.alert(" Registration Sucessfull!")
+  //     console.log("Registration Sucessfull")
+  //   }
+  // }
 
   return (
-    <>
-        <div className="ma">
-
-        <form>
-            <input type = "text" name = "username" id = "username"  placeholder='username'onChange={handleinput} value={user.username} />
-            <input type = "text" name = "password" id = "password" placeholder='password' onChange={handleinput} value={user.password} />
-            <input type = "text" name = "role" id = "role" placeholder='role'onChange={handleinput} value={user.role} /> 
-            <button onClick={register} >submit </button>
-        </form>
+    <div id='workshop'>
+        <Navbar/>
+        <div className="form-div col-lg-5 col-md-8 col-sm-12">
+          <div className="heading">
+            <h2>React JS Workshop Registration Form</h2>
+          </div>
+          <div className='form-group workshop-form'>
+              <label htmlFor="">Name</label>
+              <input className="form-control" type = "text" name = "fullanme" id = "name"  placeholder='Full Name'onChange={handleinput} value={user.fullname}  required/>
+              
+              <label htmlFor="">Branch</label>
+              <input className="form-control" type = "text" name = "branch" id = "branch" placeholder='Branch' onChange={handleinput} value={user.branch}  required/>
+              
+              <label htmlFor="">Year</label>
+              <input className="form-control" type = "text" name = "year" id = "year" placeholder='Year'onChange={handleinput} value={user.year}  required/> 
+              
+              <label htmlFor="">Email</label>
+              <input className="form-control" type = "email" name = "email" id = "email"  placeholder='Email'onChange={handleinput} value={user.email}  required/>
+              
+              <label htmlFor="">Contact No.</label>
+              <input className="form-control" type = "tel" name = "phone" id = "phone"  placeholder='Contact No.'onChange={handleinput} value={user.phone}  required/>
+              
+              <label htmlFor="">Pre Aquired Knowledge   (if don't know any thing leave blank!)</label>
+              <input className="form-control" type = "text" name = "preknowledge" id = "preknowledge" placeholder='Pre Aquired Knowledge of web-Development...' onChange={handleinput} value={user.preknowledge} />
+              <span>Insert Data Seprated with comma " , ".</span>
+              
+              <label htmlFor="">Available for WorkShop</label>
+              <select className="form-control" name="Select" id="" value={user.Select} required>
+                <option value="" selected>Select</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select>
+              <button className="btn btn-sm btn-outline-success" onClick={register} >Submit </button>
+          </div>
         </div>
-    </>
+    </div>
   )
 }
 
