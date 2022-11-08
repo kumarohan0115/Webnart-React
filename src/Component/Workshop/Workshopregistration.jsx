335afdf8ce834f09655f8d58eee9042019bbb9e1
kumarohan0115 <rohankr350@gmail.com>
2022-11-08 21:37:09 +0530

    Workshop form update

--- src/Component/Workshop/Workshopregistration.jsx
+++ src/Component/Workshop/Workshopregistration.jsx
@@ -11,14 +11,16 @@ const Workshopregistration = () => {
     fullname:"",
     branch:"",
     year:"",
+    section:"",
     email:"",
     phone:"",
     preknowledge:"",
+    attendance:"",
     option:""
   })
 
   useEffect(() => {
-    if(user.fullname ==="" || user.option ==="" || user.phone ==="" || user.email ==="" || user.year ==="" || user.branch ===""){
+    if(user.fullname ==="" || user.option ==="" || user.phone ==="" || user.email ==="" || user.year ==="" || user.branch ==="" || user.section ==="" || user.attendance ===""){
       document.getElementById("submit-btn").setAttribute('disabled',true);
     }
     else{
@@ -47,9 +49,11 @@ const Workshopregistration = () => {
       fullname:user.fullname,
       branch:user.branch,
       year:user.year,
+      section:user.section,
       email:user.email,
       phone:user.phone,
       preknowledge:user.preknowledge,
+      attendance:user.attendance,
       option:user.option
   })
   .then(function (response) {
@@ -97,8 +101,17 @@ const Workshopregistration = () => {
               <label htmlFor="">Branch</label>
               <input className="form-control" type = "text" name = "branch" id = "branch" placeholder='Branch' maxLength={3} onChange={handleinput} value={user.branch} autoCapitalize="true" required/>
               
-              <label htmlFor="">Year</label>
-              <input className="form-control" type = "number" name = "year" id = "year" placeholder='Year' maxLength={1} max={4} min={2} onChange={handleinput} value={user.year}  required/> 
+              <div className="yrr-sec">
+                <div className="input-field">
+                  <label htmlFor="">Year</label>
+                  <input className="form-control yr" type = "number" name = "year" id = "year" placeholder='Year' maxLength={1} minLength={1} max={4} min={2} onChange={handleinput} value={user.year}  required/> 
+                </div>
+                
+                <div className="input-field" style={{'marginLeft':'10px'}}>
+                  <label htmlFor="">Section</label>
+                  <input className="form-control yr" type = "text" name = "section" id = "section" placeholder='Section' maxLength={1} minLength={1} min={2} onChange={handleinput} value={user.section}  required/> 
+                </div>
+              </div>
               
               <label htmlFor="">Email</label>
               <input className="form-control" type = "email" name = "email" id = "email"  placeholder='Email'onChange={handleinput} value={user.email}  required/>
@@ -108,11 +121,24 @@ const Workshopregistration = () => {
               <span>Do not include +91-</span>
               
               <label htmlFor="">Pre Aquired Knowledge   (if don't know any thing leave blank!)</label>
-              <input className="form-control" type = "text" name = "preknowledge" id = "preknowledge" placeholder='Pre Aquired Knowledge of web-Development...' onChange={handleinput} value={user.preknowledge} />
+              <input className="form-control" type = "text" name = "preknowledge" id = "preknowledge" placeholder='Any Knowledge about web-Dev...' onChange={handleinput} value={user.preknowledge} />
               <span>Insert Data Seprated with comma " , ".</span>
               
-              <label htmlFor="">Available for WorkShop</label>
-              <input className="form-control" type = "text" name = "option" id = "option" placeholder='Available for offline Workshop' onChange={handleinput} value={user.option} />
+
+              <div className="yrr-sec">
+                <div className="input-field">
+                  <label htmlFor="">Attendance(%)</label>
+                  <input className="form-control yr" type = "text" name = "attendance" id = "attendance" placeholder='Attendance' onChange={handleinput} value={user.attendance}  required/> 
+                  <span>Attendance till 2nd Nov 2022.</span>
+                </div>
+                
+                <div className="input-field" style={{'marginLeft':'10px'}}>
+                  <label htmlFor="">Available for WorkShop</label>
+                  <input className="form-control" type = "text" name = "option" id = "option" placeholder='Available for offline Workshop' onChange={handleinput} value={user.option} />
+                </div>
+                
+              </div>
+
               
               <button className="workshop_btn btn btn-sm btn-success" id='submit-btn' onClick={register}>Submit </button>
           </div>
