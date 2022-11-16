261a220b63f56c375c1f3a7d615b6f4efe7c70ed
kumarohan0115 <rohankr350@gmail.com>
2022-11-16 22:04:42 +0530

    Update in form add few more form

--- src/Component/Credencials/DevelopersSignup.jsx
+++ src/Component/Credencials/DevelopersSignup.jsx
@@ -1,78 +1,304 @@
 import './developersSignup.css'
+import axios from '../../axios'
 
-import { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+
+
+import { useState, useEffect, useRef } from 'react';
 
 const DevelopersSignup = () => {
 
+  // const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
-  const [isChecked, setIsChecked] = useState(false);
+  const userRef = useRef();
+  const errRef = useRef();
 
-  const handleOnChange = () => {
-    setIsChecked(!isChecked);
-  };
+  const [userFocus, setUserFocus] = useState(false);
+
+
+
+  const [pass, setPass] = useState();
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState();
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(pass);
+    setValidPwd(result);
+    const match = pass === matchPwd;
+    setValidMatch(match);
+  }, [pass, matchPwd, PWD_REGEX])
+
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [pass, matchPwd])
+
+  const submitHandle = async (e) => {
+    e.preventDefault();
+    const v1 = PWD_REGEX.test(pass)
+    const v2 = PWD_REGEX.test(pass)
+    if (!v1 || !v2) {
+      setErrMsg("Invalid Entry");
+      return
+    }
+    setSuccess(true);
+  }
+
+
+  // registeration function defined==================================>
+
+  const [DevelopersData, setDevelopersData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    contact: "",
+    address: "",
+    profile: "",
+    intrestedToGetHired: ""
+  })
+
+  useEffect(() => {
+    if (DevelopersData.firstName === "" || DevelopersData.lastName === "" || DevelopersData.email === ""  || DevelopersData.contact === "" || DevelopersData.address === "" || DevelopersData.profile === "" || DevelopersData.intrested === "") {
+      document.getElementById("developersRegister_btn").setAttribute('disabled', true);
+    }
+    else {
+      console.log('entered')
+      document.getElementById("developersRegister_btn").removeAttribute('disabled');
+      console.log('exit')
+    }
+  }, [DevelopersData])
+
+
+  const disable = () => {
+    const data = document.getElementsByTagName('input');
+    if (data === "") {
+      console.log("no data found");
+    }
+    else {
+      console.log();
+    }
+  }
+  disable();
+
+
+  const devloperRegister = () => {
+    console.log(DevelopersData);
+    document.getElementById("developersRegister_btn").setAttribute('disabled', true);
+    axios.post('/developerRegistration', {
+      firstName: DevelopersData.firstName,
+      lastName: DevelopersData.lastName,
+      email: DevelopersData.email,
+      password: pass,
+      confirmPassword: matchPwd,
+      contact: DevelopersData.contact,
+      address: DevelopersData.address,
+      profile: DevelopersData.profile,
+      intrestedToGetHired: DevelopersData.intrested
+    })
+      .then(function (response) {
+        console.log(response);
+        // setSuccess(true);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
+  let developersdata, value;
+  const developerSignupInput = async (e) => {
+    console.log(e);
+    developersdata = e.target.name;
+    value = e.target.value;
+    await setDevelopersData({ ...DevelopersData, [developersdata]: value });
+  }
+
+
+  // const [isChecked, setIsChecked] = useState(false);
+
+  // const handleOnChange = () => {
+  //   setIsChecked(!isChecked);
+  // };
 
 
 
   return (
     <div id='devsignupmain'>
-        <div className="devlopersForm">
-            <form action="" className="form-group">
-              <div className="formName">
-                  <h3>Register As Developer</h3>
-                  <hr style={{'border':'2px solid white', 'backgroundColor':'white'}} />
-              </div>
-              <label htmlFor="First Name">First Name <span>*</span></label>
-              <input type="text" className='form-control' placeholder="First Name" required />
+      <div className="devlopersForm">
+        <form action="" className="form-group">
+          <div className="formName">
+            <h3>Register As Developer</h3>
+            <hr style={{ 'border': '2px solid white', 'backgroundColor': 'white' }} />
+          </div>
+          <label htmlFor="First Name">First Name <span>*</span></label>
+          <input
+            onChange={developerSignupInput}
+            type="text"
+            id="devfirstName"
+            className='form-control developers_Input'
+            placeholder="First Name"
+            value={DevelopersData.firstName}
+            name="firstName"
+            ref={userRef}
+            autoComplete="off"
+            // onChange={(e) => setFirstName(e.target.value)}
+            // aria-invalid={(validName ? "false" : "true")}
+            aria-describedby="uidnote"
+            onFocus={() => setUserFocus(true)}
+            onBlur={() => setUserFocus(false)}
+            required />
 
-              <label htmlFor="Last Name">Last Name <span>*</span></label>
-              <input type="text" className='form-control' placeholder="Last Name" required />
+          <label htmlFor="Last Name">Last Name <span>*</span></label>
+          <input
+            onChange={developerSignupInput}
+            type="text"
+            id="devlastName"
+            className='form-control developers_Input'
+            placeholder="Last Name"
+            value={DevelopersData.lastName}
+            name="lastName"
+            ref={userRef}
+            autoComplete="off"
+            // onChange={(e) => setLastName(e.target.value)}
+            // aria-invalid={(validName ? "false" : "true")}
+            aria-describedby="uidnote"
+            onFocus={() => setUserFocus(true)}
+            onBlur={() => setUserFocus(false)}
+            required />
 
-              <label htmlFor="Email">Email <span>*</span></label>
-              <input type="email" className='form-control' placeholder="Email" required />
+          <label htmlFor="Email">Email <span>*</span></label>
+          <input
+            onChange={developerSignupInput} 
+            type="email"
+            className='form-control developers_Input'
+            value={DevelopersData.email}
+            name='email'
+            placeholder="Email"
+            required
+          // onChange={(e) => setDevelopersEmail(e.target.value)}
+          />
 
-              <label htmlFor="Contact">Contact <span>*</span></label>
-              <input type="tel" className='form-control' placeholder="Contact" required />
+          <label htmlFor="Password">Password<span>*</span>
+            <span className={validPwd ? "valid" : "hide"}><FontAwesomeIcon icon={faCheck} /></span>
+            <span className={validPwd || !pass ? "hide" : "invalid"}><FontAwesomeIcon icon={faTimes} /></span>
+          </label>
+          <input
+            // onChange={developerSignupInput}
+            type="password"
+            className='form-control developers_Input'
+            minLength={6}
+            maxLength={24}
+            placeholder='Password'
+            name="password"
+            value={pass}
+            id="password"
+            required
+            onChange={(e) => setPass(e.target.value)}
+            aria-invalid={validPwd ? "false" : "true"}
+            aria-describedby="pwdnote"
+            onFocus={() => setPwdFocus(true)}
+            onBlur={() => setPwdFocus(false)}
+          />
+          <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+            <FontAwesomeIcon icon={faInfoCircle} />
+            8-24 Characters.<br />
+            Must include uppercase and lowecase ltters, a number and a spcial character.<br />
+            Allowed special Characters: eg. @,!,&,$,#,% etc.
+          </p>
 
-              <label htmlFor="Address">Address <span>*</span></label>
-              <input type="text" className='form-control' placeholder="Address" required />
+          <label htmlFor="Confirm Password">Confirm Password<span>*</span>
+            <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
+            <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
+          </label>
+          <input
+            // onChange={developerSignupInput}
+            type="password"
+            className='form-control developers_Input'
+            placeholder='Confirm Password'
+            value={matchPwd}
+            id="confirm_pwd"
+            name="confirmPassword"
+            onChange={(e) => setMatchPwd(e.target.value)}
+            required
+            aria-invalid={validMatch ? "false" : "true"}
+            aria-describedby="confirmnote"
+            onFocus={() => setMatchFocus(true)}
+            onBlur={() => setMatchFocus(false)}
+          />
 
-              <label htmlFor="profile_Choice">Choose Your Profile <span>*</span></label>
-              <select name="profileOption" id="profileoption" className='form-control'>
-                <option value="">Select</option>
-                <option value="Web Developer">Web Developer</option>
-                <option value="UI/UX Designer">UI/UX Designer</option>
-                <option value="Game Developer">Game Developer</option>
-                <option value="Game Artist">Game Artist</option>
-                <option value="Pixel Artist">Pixel Artist</option>
-                <option value="Photographer">Photographer</option>
-                <option value="Other">Other</option>
-              </select>
+          <label htmlFor="Contact">Contact <span>*</span></label>
+          <input
+            onChange={developerSignupInput}
+            type="tel"
+            className='form-control developers_Input'
+            placeholder="Contact"
+            value={DevelopersData.contact}
+            name='contact'
+            ref={userRef}
+            autoComplete='off'
+            required />
 
+          <label htmlFor="Address">Address <span>*</span></label>
+          <input
+            onChange={developerSignupInput}
+            type="text"
+            className='form-control developers_Input'
+            value={DevelopersData.address}
+            name='address'
+            placeholder="Address"
+            required />
 
-              <div className="intrestedToGetHired">
-                <label htmlFor="intrestedToGetHired">Intrested To Get Hired</label>
-                <input
-                  type="checkbox"
-                  id="intrested"
-                  name="intrested"
-                  value="intrested"
-                  checked={isChecked}
-                  onChange={handleOnChange}
-                />
-              </div>
+          <label htmlFor="profile_Choice">Choose Your Profile <span>*</span></label>
+          <select id="profileoption" className='form-control developers_Input' name="profile">
+            <option value="">Select</option>
+            <option value="Web Developer">Web Developer</option>
+            <option value="UI/UX Designer">UI/UX Designer</option>
+            <option value="Game Developer">Game Developer</option>
+            <option value="Game Artist">Game Artist</option>
+            <option value="Pixel Artist">Pixel Artist</option>
+            <option value="Photographer">Photographer</option>
+            <option value="Other">Other</option>
+          </select>
 
-              <div className="profilePicture">
-                <label htmlFor="">Profile Picture</label>
-                <label htmlFor="images" className="drop-container">
-                  <span className="drop-title">Drop files here</span>
-                  <h5>or</h5>
-                  <input type="file" id="images" accept="image/*" required/>
-                </label>
-              </div>
+          <div className="intrestedToGetHired">
+            <label htmlFor="intrestedToGetHired">Intrested To Get Hired</label>
+            <input
+              // onChange={developerSignupInput}
+              type="checkbox"
+              id="intrested"
+              value={DevelopersData.intrested}
+              name="intrested"
+              // checked={isChecked}
+              // onChange={handleOnChange}
+            />
+          </div>
 
-              <button className='btn btn-sm btn-primary developerRegister form-control'>Register</button>
-            </form>
-        </div>
+          <div className="profilePicture">
+            <label htmlFor="">Profile Picture</label>
+            <label htmlFor="images" className="drop-container">
+              <span className="drop-title">Drop files here</span>
+              <h5>or</h5>
+              <input
+                onChange={developerSignupInput} type="file" id="images" accept="image/*" />
+            </label>
+          </div>
+
+          <button id='developersRegister_btn' className='btn btn-sm btn-primary form-control' disabled onClick={devloperRegister}>Register</button>
+        </form>
+      </div>
     </div>
   )
 }
--- src/Component/Workshop/Workshopregistration.jsx
+++ src/Component/Workshop/Workshopregistration.jsx
@@ -16,11 +16,10 @@ const Workshopregistration = () => {
     phone:"",
     preknowledge:"",
     attendance:"",
-    option:""
   })
 
   useEffect(() => {
-    if(user.fullname ==="" || user.option ==="" || user.phone ==="" || user.email ==="" || user.year ==="" || user.branch ==="" || user.section ==="" || user.attendance ===""){
+    if(user.fullname ==="" || user.phone ==="" || user.email ==="" || user.year ==="" || user.branch ==="" || user.section ==="" || user.attendance ===""){
       document.getElementById("submit-btn").setAttribute('disabled',true);
     }
     else{
@@ -53,11 +52,10 @@ const Workshopregistration = () => {
       email:user.email,
       phone:user.phone,
       preknowledge:user.preknowledge,
-      attendance:user.attendance,
-      option:user.option
+      attendance:user.attendance
   })
   .then(function (response) {
-    console.log(response);
+    // console.log(response);
     setSuccess(true);
   })
   .catch(function (error) {
@@ -68,7 +66,7 @@ const Workshopregistration = () => {
 
   let name,value;
   const handleinput=async(e)=>{
-    console.log(e);
+    // console.log(e);
     name=e.target.name;
     value=e.target.value;
     await setUser({...user,[name]:value});
@@ -80,12 +78,17 @@ const Workshopregistration = () => {
     document.getElementsByClassName('popup')[0].style.display="none"
   }
 
+
+  const rangecss={
+    color:'white'
+  }
+
   return (
     <div id='workshop'>
         <Navbar/>
         {
         success ?(
-            <section>
+            <section style={{'text-align':'center'}}>
                 <h1>Submitted Successfully!</h1>
             </section>
         ):
@@ -95,49 +98,40 @@ const Workshopregistration = () => {
             <h2>React Js Workshop Registration</h2>
           </div>
           <div className='form-group workshop-form'>
-              <label htmlFor="">Name</label>
-              <input className="form-control" type = "text" name = "fullname" id = "name"  placeholder='Full Name'onChange={handleinput} value={user.fullname}  required/>
+              <label htmlFor="">Name <span>*</span></label>
+              <input className="form-control" type = "text" name = "fullname" id = "name"  autoComplete='off' placeholder='Full Name'onChange={handleinput} value={user.fullname}  required/>
               
-              <label htmlFor="">Branch</label>
-              <input className="form-control" type = "text" name = "branch" id = "branch" placeholder='Branch' maxLength={3} onChange={handleinput} value={user.branch} autoCapitalize="true" required/>
+              <label htmlFor="">Branch <span>*</span></label>
+              <input className="form-control" type = "text" name = "branch" id = "branch" autoComplete='off' placeholder='Branch' maxLength={3} onChange={handleinput} value={user.branch} autoCapitalize="true" required/>
               
               <div className="yrr-sec">
                 <div className="input-field">
-                  <label htmlFor="">Year</label>
-                  <input className="form-control yr" type = "number" name = "year" id = "year" placeholder='Year' maxLength={1} minLength={1} max={4} min={2} onChange={handleinput} value={user.year}  required/> 
+                  <label htmlFor="">Year <span>*</span></label>
+                  <input className="form-control yr" type = "number" name = "year" id = "year" autoComplete='off' placeholder='Year' maxLength={1} minLength={1} max={4} min={2} onChange={handleinput} value={user.year}  required/> 
                 </div>
                 
                 <div className="input-field" style={{'marginLeft':'10px'}}>
-                  <label htmlFor="">Section</label>
-                  <input className="form-control yr" type = "text" name = "section" id = "section" placeholder='Section' maxLength={1} minLength={1} min={2} onChange={handleinput} value={user.section}  required/> 
+                  <label htmlFor="">Section <span>*</span></label>
+                  <input className="form-control yr" type = "text" name = "section" id = "section" autoComplete='off' placeholder='Section' maxLength={1} minLength={1} min={2} onChange={handleinput} value={user.section}  required/> 
                 </div>
               </div>
               
-              <label htmlFor="">Email</label>
-              <input className="form-control" type = "email" name = "email" id = "email"  placeholder='Email'onChange={handleinput} value={user.email}  required/>
+              <label htmlFor="">Email <span>*</span></label>
+              <input className="form-control" type = "email" name = "email" id = "email"  autoComplete='off' placeholder='Email'onChange={handleinput} value={user.email}  required/>
               
-              <label htmlFor="">Contact No.</label>
-              <input className="form-control" type = "tel" name = "phone" id = "phone"  placeholder='10 Digit Contact No.'onChange={handleinput} value={user.phone} minLength={10} maxLength={10} required/>
+              <label htmlFor="">Contact No. <span>*</span></label>
+              <input className="form-control" type = "tel" name = "phone" id = "phone"  autoComplete='off' placeholder='10 Digit Contact No.'onChange={handleinput} value={user.phone} minLength={10} maxLength={10} required/>
               <span>Do not include +91-</span>
               
               <label htmlFor="">Pre Aquired Knowledge   (if don't know any thing leave blank!)</label>
-              <input className="form-control" type = "text" name = "preknowledge" id = "preknowledge" placeholder='Any Knowledge about web-Dev...' onChange={handleinput} value={user.preknowledge} />
+              <input className="form-control" type = "text" name = "preknowledge" id = "preknowledge" autoComplete='off' placeholder='Any Knowledge about web-Dev...' onChange={handleinput} value={user.preknowledge} />
               <span>Insert Data Seprated with comma " , ".</span>
               
-
-              <div className="yrr-sec">
-                <div className="input-field">
-                  <label htmlFor="">Attendance(%)</label>
-                  <input className="form-control yr" type = "text" name = "attendance" id = "attendance" placeholder='Attendance' onChange={handleinput} value={user.attendance}  required/> 
-                  <span>Attendance till 2nd Nov 2022.</span>
-                </div>
+              <label htmlFor="">Attendance(%) <span>*</span></label>
+              <input className="form-control attend" type = "range" name = "attendance" id = "attendance" autoComplete='off' placeholder='Attendance' onChange={handleinput} value={user.attendance}  required/> 
+              <h4 id='valuee' style={rangecss}>{user.attendance} %</h4>
+              <span>Attendance till 2nd Nov 2022.</span>
                 
-                <div className="input-field" style={{'marginLeft':'10px'}}>
-                  <label htmlFor="">Available for WorkShop</label>
-                  <input className="form-control" type = "text" name = "option" id = "option" placeholder='Available for offline Workshop' onChange={handleinput} value={user.option} />
-                </div>
-                
-              </div>
 
               
               <button className="workshop_btn btn btn-sm btn-success" id='submit-btn' onClick={register}>Submit </button>
